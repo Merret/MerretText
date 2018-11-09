5ceb23ab4b33a515a0b0a3d040d16ab9b87ee61d
JacobLinCool <jacoblin2011@gmail.com>
2018-11-09 23:01:33 +0800

    Update indent.js

--- js/indent.js
+++ js/indent.js
@@ -1,7 +1,27 @@
 function indent(code) {
-    code = code.replace(/\s+/g, "");
     var json = function() {
-        
+        var reg = /(\[|\]|\{|\}|\"|\')/g;
+        var last = 0, tab = 0;
+        while(code.indexOf(reg, last)!=-1) {
+            var index = code.indexOf(reg, last);
+            last = code.indexOf(reg, last)+1 < code.length ? code.indexOf(reg, last)+1 : code.length;
+            var chr = code.charAt(code.indexOf(reg, last));
+            if(chr == "[") {
+                code.replace(index, )
+                ++tab;
+            } else if(chr == "{") {
+                
+                ++tab;
+            } else if(chr == "]") {
+                
+                --tab;
+            } else if(chr == "}") {
+                
+                --tab;
+            } else if(chr == "\"" || chr == "\'") {
+                
+            }
+        }
     };
     return {
         "json" : json
