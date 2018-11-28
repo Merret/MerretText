70e58947205c81980b2bc9782f00cf41a465aac1
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 22:07:21 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -5,13 +5,14 @@ if(typeof xhr === 'undefined') {
     script.onload = function() {getUserInfo();};
     document.head.appendChild(script);
 }
+console.log("Getting userInfo: Prepared 1/2");
 if(typeof UAParser === 'undefined') {
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/gh/faisalman/ua-parser-js@0.7.19/dist/ua-parser.min.js';
-    script.onload = function() {userInfo.device = UAParser(navigator.userAgent);};
+    script.onload = function() {userInfo.device = UAParser(navigator.userAgent);console.log("Getting userInfo: Finished.");};
     document.head.appendChild(script);
-
 }
+console.log("Getting userInfo: Prepared 2/2");
 function getUserInfo() {
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
 var keyListIndex = Math.floor(Math.random()*3);
@@ -22,4 +23,5 @@ function DNSkey(length, chars) {
     return result;
 }
 userInfo.dnsServer = JSON.parse(xhr.get("https://"+DNSkey(32, '0123456789abcdefghijklmnopqrstuvwxyz')+".edns.ip-api.com/json")).dns;
+console.log("Getting userInfo: Analysing...");
 }
