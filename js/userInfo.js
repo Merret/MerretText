84b60754c656ca3ee7d348edfe5a96b511d34a47
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 22:01:52 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -1,6 +1,7 @@
 if(typeof xhr === 'undefined') {
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/gh/Merret/UsefulJS@0.4.2/js/xhr.min.js';
+    script.onload = function() {getUserInfo();};
     document.head.appendChild(script);
 }
 if(typeof UAParser === 'undefined') {
@@ -10,6 +11,7 @@ if(typeof UAParser === 'undefined') {
     document.head.appendChild(script);
 
 }
+function getUserInfo() {
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
 var keyListIndex = Math.floor(Math.random()*3);
 var userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex]));
@@ -19,3 +21,4 @@ function DNSkey(length, chars) {
     return result;
 }
 userInfo.dnsServer = JSON.parse(xhr.get("https://"+DNSkey(32, '0123456789abcdefghijklmnopqrstuvwxyz')+".edns.ip-api.com/json")).dns;
+}
