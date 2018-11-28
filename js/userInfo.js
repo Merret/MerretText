7d7063897980da3758e35a23a6bba0082df78e3f
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 21:36:06 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -1,3 +1,15 @@
+if(!xhr) {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/Merret/UsefulJS@0.4.2/js/xhr.min.js';
+    document.head.appendChild(script);
+}
+if(!UAParser) {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/faisalman/ua-parser-js@0.7.19/dist/ua-parser.min.js';
+    script.onload = function() {userInfo.device = UAParser(navigator.userAgent);};
+    document.head.appendChild(script);
+
+}
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
 var keyListIndex = Math.floor(Math.random()*3);
 var userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex]));
@@ -7,8 +19,3 @@ function DNSkey(length, chars) {
     return result;
 }
 userInfo.dnsServer = JSON.parse(xhr.get("https://"+DNSkey(32, '0123456789abcdefghijklmnopqrstuvwxyz')+".edns.ip-api.com/json")).dns;
-
-var script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/gh/faisalman/ua-parser-js@0.7.19/dist/ua-parser.min.js';
-script.onload = function() {userInfo.device = UAParser(navigator.userAgent);};
-document.head.appendChild(script);
