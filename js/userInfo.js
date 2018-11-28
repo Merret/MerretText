3a2d918a62055daa8a0531be2f70b439e86fed6e
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 21:27:50 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -1,5 +1,5 @@
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
-var keyListIndex = Math.floor((Math.random()*3)+1);
+var keyListIndex = Math.floor(Math.random()*3);
 var userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex]));
 function DNSkey(length, chars) {
     var result = '';
