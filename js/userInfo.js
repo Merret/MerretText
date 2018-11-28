edd25fd6bcc4da243336b51bcaa028b29f9d0ec1
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 22:02:43 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -1,3 +1,4 @@
+var userInfo;
 if(typeof xhr === 'undefined') {
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/gh/Merret/UsefulJS@0.4.2/js/xhr.min.js';
@@ -14,7 +15,7 @@ if(typeof UAParser === 'undefined') {
 function getUserInfo() {
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
 var keyListIndex = Math.floor(Math.random()*3);
-var userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex]));
+userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex]));
 function DNSkey(length, chars) {
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
